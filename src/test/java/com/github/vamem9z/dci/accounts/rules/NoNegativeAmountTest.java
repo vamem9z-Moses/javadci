1598e0b0157e3c9374ce7d55329d2a33b5d7160e
Moses E. Miles III <vamem9z@gmail.com>
2015-07-16 08:32:29 -0400

    Add missing test for accounts no negative amount rule

--- src/test/java/com/github/vamem9z/dci/accounts/rules/NoNegativeAmountTest.java
+++ src/test/java/com/github/vamem9z/dci/accounts/rules/NoNegativeAmountTest.java
@@ -1,18 +1,50 @@
 package test.java.com.github.vamem9z.dci.accounts.rules;
 
-import org.testng.annotations.Test;
+import static org.testng.Assert.assertEquals;
+import static test.java.com.github.vamem9z.dci.accounts.TestAccountHelpers.makeCheckingAccount;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.stream.Stream;
+
 import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+import main.java.com.github.vamem9z.dci.accounts.contexts.AccountDepositContext;
+import main.java.com.github.vamem9z.dci.accounts.contexts.AccountWithDrawContext;
+import main.java.com.github.vamem9z.dci.accounts.rules.NoNegativeAmountsRule;
+import main.java.com.github.vamem9z.dci.contexts.Contexter;
+import main.java.com.github.vamem9z.dci.contexts.results.ContextResult;
+import main.java.com.github.vamem9z.dci.contexts.results.Success;
+import main.java.com.github.vamem9z.dci.contexts.results.WrongContext;
+import main.java.com.github.vamem9z.dci.contexts.results.accounts.NegativeAmountNotAllowed;
 
 public class NoNegativeAmountTest {
-  @Test(dataProvider = "dp")
-  public void f(Integer n, String s) {
+  @Test(groups = {"unit"}, dataProvider = "dp")
+  public void f(Contexter ctx, ContextResult expectedResult, String msg) {
+	  NoNegativeAmountsRule rule = new NoNegativeAmountsRule();
+	  ContextResult result = rule.action(ctx);
+	  assertEquals(result, expectedResult, msg);
   }
 
   @DataProvider
   public Object[][] dp() {
     return new Object[][] {
-      new Object[] { 1, "a" },
-      new Object[] { 2, "b" },
-    };
+      new Object[] { new EmptyTestContext(), new WrongContext(), "Test sending wrong context"},
+      new Object[] { new AccountWithDrawContext(makeCheckingAccount(100.00), 50.00, "Test withdraw"), 
+    		  new Success(), "Test sending withdraw context"},
+      new Object[] { new AccountDepositContext(makeCheckingAccount(100.00), 50.00, "Test deposit"), 
+    		  new Success(), "Test sending deposit context"},
+      new Object[] { new AccountWithDrawContext(makeCheckingAccount(100.00), -50.00, "Test withdraw"), 
+    		  new NegativeAmountNotAllowed(), "Test sending withdraw context with negative amount"},
+      };
+    }
+  
+ private class EmptyTestContext implements Contexter {
+	  public EmptyTestContext() {}
+	  public Stream<ContextResult>execute()  {
+		  ArrayList<ContextResult> result = new ArrayList<ContextResult>(Arrays.asList(new Success()));
+		  return result.stream();
+	  }
   }
 }
