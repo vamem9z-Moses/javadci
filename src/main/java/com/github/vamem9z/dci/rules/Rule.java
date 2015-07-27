f8b5820ea078bba35e6675e3e604bb418255f6cb
Moses E. Miles III <vamem9z@gmail.com>
2015-07-27 15:31:19 -0400

    Add missing Functional Interface annotation for Rule

--- src/main/java/com/github/vamem9z/dci/rules/Rule.java
+++ src/main/java/com/github/vamem9z/dci/rules/Rule.java
@@ -3,6 +3,7 @@ package main.java.com.github.vamem9z.dci.rules;
 import main.java.com.github.vamem9z.dci.usecases.UseCase;
 import main.java.com.github.vamem9z.dci.usecases.results.UseCaseResult;
 
+@FunctionalInterface
 public interface Rule {
 	UseCaseResult action(UseCase ctx);
 }
