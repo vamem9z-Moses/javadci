57c7ddfd36ce332221ec1f130ce7e20ebf8251e2
Moses E. Miles III <vamem9z@gmail.com>
2015-08-02 13:27:03 -0400

    Remove lombok from AccountsModule to eliminate magic

--- src/main/java/com/github/vamem9z/dci/accounts/rules/AccountProtectionRule.java
+++ src/main/java/com/github/vamem9z/dci/accounts/rules/AccountProtectionRule.java
@@ -1,14 +1,15 @@
 package com.github.vamem9z.dci.accounts.rules;
 
-import lombok.NoArgsConstructor;
 import com.github.vamem9z.dci.accounts.usecases.AccountWithdrawUseCase;
 import com.github.vamem9z.dci.core.rules.Rule;
 import com.github.vamem9z.dci.core.usecases.UseCase;
 import com.github.vamem9z.dci.core.usecases.results.UseCaseResult;
 import com.github.vamem9z.dci.core.usecases.results.general.Successful;
 
-@NoArgsConstructor
 public final class AccountProtectionRule implements Rule {
+
+	public AccountProtectionRule() {}
+	
 	@Override
 	public UseCaseResult action(UseCase ctx) {
 		if (ctx instanceof AccountWithdrawUseCase) {
--- src/main/java/com/github/vamem9z/dci/accounts/rules/NoNegativeAmountsRule.java
+++ src/main/java/com/github/vamem9z/dci/accounts/rules/NoNegativeAmountsRule.java
@@ -1,6 +1,5 @@
 package com.github.vamem9z.dci.accounts.rules;
 
-import lombok.NoArgsConstructor;
 import com.github.vamem9z.dci.accounts.usecases.ChangeBalanceUseCase;
 import com.github.vamem9z.dci.core.rules.Rule;
 import com.github.vamem9z.dci.core.usecases.UseCase;
@@ -9,8 +8,9 @@ import com.github.vamem9z.dci.core.usecases.results.accounts.NegativeAmountNotAl
 import com.github.vamem9z.dci.core.usecases.results.general.Successful;
 import com.github.vamem9z.dci.core.usecases.results.general.WrongContext;
 
-@NoArgsConstructor
 public class NoNegativeAmountsRule implements Rule {
+	
+	public NoNegativeAmountsRule() {}
 
 	@Override
 	public final UseCaseResult action(UseCase ctx) {
