5b03334bd96ee36ca9fad2db3c8e07244caa000e
Moses E. Miles III <vamem9z@gmail.com>
2015-08-05 09:54:13 -0400

    Add Model bound to limit return value from creatModelBuilder

--- src/main/java/com/github/vamem9z/dci/core/domains/Persister.java
+++ src/main/java/com/github/vamem9z/dci/core/domains/Persister.java
@@ -1,8 +1,9 @@
 package com.github.vamem9z.dci.core.domains;
 
+import com.github.vamem9z.dci.core.data.models.Model;
 import com.github.vamem9z.dci.core.data.models.ModelBuilder;
 
-public interface Persister<U extends ModelBuilder<?>> {
+public interface Persister<U extends ModelBuilder<? extends Model>> {
 	/**
 	 * Gives domain objects the ability to persist and retrieve themselves from the data store.
 	 * <p>
