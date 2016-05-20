f630ea6df1685334e1a83ec32f48ce2a09dcf723
Moses E. Miles III <vamem9z@gmail.com>
2016-05-20 19:32:35 -0400

    Add insufficient funds to paybills

--- src/test/java/com/github/vamem9z/dci/accounts/usecases/PayBillsUseCaseTest.java
+++ src/test/java/com/github/vamem9z/dci/accounts/usecases/PayBillsUseCaseTest.java
@@ -12,6 +12,7 @@ import com.github.vamem9z.dci.accounts.AccountTest;
 import com.github.vamem9z.dci.accounts.roles.AccountRole;
 import com.github.vamem9z.dci.accounts.roles.TransferMoneySourceRole;
 import com.github.vamem9z.dci.core.domains.results.Result;
+import com.github.vamem9z.dci.core.domains.results.accounts.InsufficientBalance;
 import com.github.vamem9z.dci.core.domains.results.general.Successful;
 import com.github.vamem9z.dci.core.usecases.UseCaseTest;
 
@@ -21,14 +22,20 @@ public final class PayBillsUseCaseTest implements UseCaseTest, AccountTest {
 	private final ArrayList<AccountRole> creditors = new ArrayList<AccountRole>(
 			Arrays.asList(makeVendorAccount(394.30), makeVendorAccount(122.12)));
 
+	private final ArrayList<AccountRole> creditorsMoreThanAvailable = new ArrayList<AccountRole>(
+			Arrays.asList(makeVendorAccount(394.30), makeVendorAccount(522.12)));
+
 	@DataProvider(name="testData")
 	public final Object[][] testData() {
 		return new Object[][] {
 			new Object[]{makeCheckingAccount(1000.34), creditors,
 					new ArrayList<Result>(Arrays.asList(new Successful(), new Successful())),
-					483.92, 0, 0, "2 liability accounts"},
-		};
-	}
+					483.92, 0, 0, "2 paybill pay accounts"},
+			new Object[]{makeCheckingAccount(600.34), creditorsMoreThanAvailable,
+					new ArrayList<Result>(Arrays.asList(new Successful(), new Successful(), new InsufficientBalance())),
+					206.04, 0, 522.12, "2 bill pay accounts not enough funds"},
+			};
+		}
 
 	@Test(dataProvider="testData", groups={"unit"})
 	public final void test(TransferMoneySourceRole sourceAccount, ArrayList<AccountRole> creditors, ArrayList<Result> expectedResult,
